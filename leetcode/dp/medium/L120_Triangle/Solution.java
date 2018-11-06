27ec94bfc34c5469d121c60d5ee6761278f1c3a5
Hefeng Cui <hefengc@amazon.com>
2018-11-06 00:09:38 -0800

    add some dp problems

---
+++ leetcode/dp/medium/L120_Triangle/Solution.java
@@ -0,0 +1,37 @@
+import java.util.List;
+
+public class Solution {
+
+    // Time Complexity: O(N^2), Space Complexity: O(N^2)
+    public int minimumTotal(List<List<Intgeger>> triangle) {
+        int n = triangle.size();
+        int[][] d = int[n][n];
+
+        for (int j = 0; j < n; j++) {
+            d[n-1][j] = triangle.get(n-1).get(j);
+        }
+
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = i; j >= 0; j--) {
+                d[i][j] = triangle.get(i).get(j) + Math.min(d[i+1][j], d[i+1][j+1]);
+            }
+        }
+        return d[0][0];
+    }
+
+    // Time Complexity: O(N^2), Space Complexity: O(N^2)
+    public int mimTotal(List<List<Integer>> triangle) {
+        int n = triangle.size();
+        int[] d = int[n];
+
+        for (int i = 0; i < n; i++) {
+            d[i] = triangle.get(n-1).get(i);
+        }
+
+        for (int i = n-2; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                dp[j] = triangle.get(i).get(j) + Math.min(dp[j], dp[j+1]);
+            }
+        }
+    }
+}
